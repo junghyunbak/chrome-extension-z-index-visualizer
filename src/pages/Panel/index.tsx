95cfe694244deb7c244b870ae65d0834d80fe4a1
junghyunbak <jeong5728@gmail.com>
2023-07-09 22:02:18 +0900

    fix: store 준비되면 렌더링하도록 수정 (+ css파일 삭제)

--- src/pages/Panel/index.tsx
+++ src/pages/Panel/index.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-
 import Panel from './Panel';
 
 import { createStoreProxy } from '../../store';
 import PortNames from '../../types/PortNames';
 
-import './index.css';
-
 const proxyStore = createStoreProxy(PortNames.ContentPort);
 
-const container = document.getElementById('app-container');
-const root = createRoot(container!);
-root.render(
-  <Provider store={proxyStore}>
-    <Panel />
-  </Provider>
-);
+proxyStore.ready().then(() => {
+  const container = document.getElementById('app-container');
+  const root = createRoot(container!);
+  root.render(
+    <Provider store={proxyStore}>
+      <Panel />
+    </Provider>
+  );
+});
