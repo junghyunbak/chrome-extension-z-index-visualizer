d82b413b54a0a75487d862382ffc834e8994304f
junghyunbak <jeong5728@gmail.com>
2023-07-09 23:23:08 +0900

    feat: 원 위치로 이동하는 버튼 추가

--- src/pages/Panel/Panel.tsx
+++ src/pages/Panel/Panel.tsx
@@ -5,10 +5,21 @@ import { ThreeDimPlane } from './components/ThreeDimPlane';
 
 import { Global } from '@emotion/react';
 import * as S from './Panel.styles';
+import { FitButton } from './components/FitButton';
 
 function Panel() {
   const layout = useRef<HTMLDivElement | null>(null);
   const drag = useRef<HTMLDivElement | null>(null);
+
+  const setPlaneToStartLoc = () => {
+    const $drag = drag.current;
+
+    if ($drag instanceof HTMLDivElement) {
+      $drag.style.top = '0px';
+      $drag.style.left = '0px';
+    }
+  };
+
   /**
    * 3차원 좌표평면 드래그 이벤트
    */
@@ -56,6 +67,8 @@ function Panel() {
   return (
     <div css={S.layout} ref={layout}>
       <Global styles={S.global} />
+
+      <FitButton setPlaneToStartLoc={setPlaneToStartLoc} />
 
       <div css={S.dragWrapper} ref={drag}>
         <ThreeDimPlane />
---
+++ src/pages/Panel/components/FitButton/index.styles.ts
@@ -0,0 +1,24 @@
+import { css } from '@emotion/react';
+
+export const layout = css`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+
+  width: 10%;
+  height: 10%;
+
+  padding: 1%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  cursor: pointer;
+
+  svg {
+    path {
+      fill: #a2b8d1;
+    }
+  }
+`;
