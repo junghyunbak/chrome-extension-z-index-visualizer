af8fd44eb2873a5df3314a56a8ce645bb9aaef9c
junghyunbak <jeong5728@gmail.com>
2023-07-10 17:00:57 +0900

    style: fit button 스타일 변경

--- src/pages/Panel/components/FitButton/index.styles.ts
+++ src/pages/Panel/components/FitButton/index.styles.ts
@@ -12,14 +12,14 @@ export const layout = css`
 export const button = css`
   box-shadow: rgba(15, 15, 15, 0.016) 0px 0px 0px 1px,
     rgba(15, 15, 15, 0.03) 0px 3px 6px, rgba(15, 15, 15, 0.06) 0px 9px 24px;
-
-  background-color: white;
+  background-color: rgb(251, 251, 250);
 
   padding: 1em;
   margin: 0 1em 1em 0;
 
   border: none;
   border-radius: 10px;
+  border: 1px solid rgb(233, 233, 231);
 
   display: flex;
   justify-content: center;
