0fc8360a2bca68a1cc46db5c2b4d08fd16ef9170
junghyunbak <jeong5728@gmail.com>
2023-07-14 17:06:18 +0900

    feat: 현재 주소의 상태 나타내는 컴포넌트 고도화

--- src/components/Address/index.tsx
+++ src/components/Address/index.tsx
@@ -1,10 +1,47 @@
 /** @jsxImportSource @emotion/react */
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { css } from '@emotion/react';
 import { useAppSelector } from '@/hooks/useAppDispatch';
 
 export function Address() {
-  const href = useAppSelector((state) => state.content.currentHref);
+  const contentHref = useAppSelector((state) => state.content.currentHref);
+
+  const [currentUrl, setCurrentUrl] = useState<string>('');
+
+  const myTabId = useRef<number>(-1);
+
+  const isLocalhost = () => {
+    return /http:\/\/(localhost|127.0.0.1):[0-9]*\/.*/.test(currentUrl);
+  };
+
+  const isSameUrl = () => {
+    return currentUrl === contentHref;
+  };
+
+  useEffect(() => {
+    /**
+     * 현재 탭의 정보를 초기화
+     * https://stackoverflow.com/questions/6718256/how-do-you-use-chrome-tabs-getcurrent-to-get-the-page-object-in-a-chrome-extensi
+     */
+    chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+      const { id, url } = tabs[0];
+
+      setCurrentUrl(url || '');
+      myTabId.current = id || -1;
+    });
+
+    /**
+     * 탭이 업데이트 될 때 마다 url를 갱신
+     * 단, 현재 탭의 업데이트에 대해서만 갱신
+     */
+    chrome.tabs.onUpdated.addListener((tabId, _, tab) => {
+      if (myTabId.current !== tabId) {
+        return;
+      }
+
+      setCurrentUrl(tab.url || '');
+    });
+  }, []);
 
   return (
     <div
@@ -13,9 +50,82 @@ export function Address() {
         top: 0;
         left: 0;
         z-index: 5;
+
+        table {
+          border-collapse: collapse;
+        }
+
+        table,
+        tr,
+        th,
+        td {
+          border: 1px solid black;
+          background-color: white;
+        }
       `}
     >
-      {href === '' ? 'No data. The web page needs to be refreshed.' : href}
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>url</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>current page</th>
+            <td>
+              {!isLocalhost() ? (
+                <p
+                  css={css`
+                    text-decoration: line-through;
+                  `}
+                >
+                  {currentUrl}
+                </p>
+              ) : (
+                <p>{currentUrl}</p>
+              )}
+            </td>
+          </tr>
+          <tr>
+            <th>content source</th>
+            <td>{contentHref}</td>
+          </tr>
+          <tr>
+            <th>state</th>
+            <td>
+              {!isLocalhost() ? (
+                <p>
+                  <StateBullet type={'red'} /> Works only on localhost.
+                </p>
+              ) : !isSameUrl() ? (
+                <p>
+                  <StateBullet type={'orange'} /> A page refresh is required.
+                </p>
+              ) : (
+                <p>
+                  <StateBullet type={'green'} /> ok
+                </p>
+              )}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+  );
+}
+
+function StateBullet({ type }: { type: 'red' | 'orange' | 'green' }) {
+  return (
+    <div
+      css={css`
+        display: inline-block;
+        width: 0.5rem;
+        height: 0.5rem;
+        border-radius: 50%;
+        background-color: ${type};
+      `}
+    />
   );
 }
