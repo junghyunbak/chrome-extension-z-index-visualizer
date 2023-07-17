c574a46dd05bdd6118aad084f66bf3e610718def
junghyunbak <jeong5728@gmail.com>
2023-07-17 21:12:15 +0900

    chore: 리팩토링 할 내용 주석으로 작성

--- src/utils/chrome.ts
+++ src/utils/chrome.ts
@@ -1,3 +1,12 @@
+/*
+TODO: 테스트 코드 작성 후 아래의 코드로 리팩토링
+
+export const getPrivousState = async () => {
+  const storage = await chrome.storage.local.get();
+
+  return storage.state;
+};
+*/
 export const getPrivousState = async () => {
   const previousState = await new Promise((resolve) => {
     chrome.storage.local.get('state', (storage) => {
